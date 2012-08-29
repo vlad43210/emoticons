943d91a823bf326ac71656831a8b7a7a92c2e53c
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-08-29 16:29:23 -0400

    adding emoticon frequency normalization

--- get_emoticon_propagation_curves.py
+++ get_emoticon_propagation_curves.py
@@ -61,12 +61,12 @@ def getEmoticonPropagationCurves(searcher, analyzer):
         if i == len(sorted_daytslist)-1: continue
         parsed_daytts = QueryParser.escape(str(sorted_dayts))
         parsed_nextdaytts = QueryParser.escape(str(sorted_daytslist[i+1]))
-        print "parsed_daytts: ", parsed_daytts, " parsed_nextdaytts: ", parsed_nextdaytts
+        #print "parsed_daytts: ", parsed_daytts, " parsed_nextdaytts: ", parsed_nextdaytts
         range_filter = RangeFilter("timestamp", parsed_daytts, parsed_nextdaytts, True, True)
         all_docs_query = MatchAllDocsQuery()
         tweets_in_range_search = searcher.search(all_docs_query, range_filter)
         num_tweets_in_range = tweets_in_range_search.length()
-        print "num tweets in range: ", num_tweets_in_range
+        #print "num tweets in range: ", num_tweets_in_range
         emoticon_propagation_hash[daytshash[sorted_dayts]]['total tweets'] = num_tweets_in_range
         
         
