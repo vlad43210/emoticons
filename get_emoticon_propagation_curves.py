5d4b889119f7ab3a3627ff916eb7f53d62570ec8
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-09-19 20:02:04 -0400

    controlling for http emoticons in search

--- get_emoticon_propagation_curves.py
+++ get_emoticon_propagation_curves.py
@@ -13,7 +13,7 @@ def getEmoticonPropagationCurves(emoticon, searcher, analyzer):
     emoticon_file_name = raw_stats_dir + normalizeEmoticonName(emoticon).rstrip('_')+".timehash"
     print "Searching for: ", emoticon, " at: ", time.time()
     escaped_emoticon = QueryParser.escape(emoticon)
-    query = QueryParser("text", analyzer).parse(escaped_emoticon)
+    query = QueryParser("emoticons", analyzer).parse(escaped_emoticon)
     hits = searcher.search(query)
     print "%s total matching documents." % hits.length()
     if hits.length() == 0: return
