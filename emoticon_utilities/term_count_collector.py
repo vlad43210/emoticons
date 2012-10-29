454ae3deb103b443cfeaae7af3c4ef4c187b928e
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-10-29 16:24:54 -0400

    debugging emoticon pmis

--- emoticon_utilities/term_count_collector.py
+++ emoticon_utilities/term_count_collector.py
@@ -20,7 +20,7 @@ class TermCountCollector(PythonHitCollector):
     def acceptsDocsOutOfOrder(self):
         return True
 
-    def collect(self, arg0):
+    def collect(self, arg0, score):
         tv = self.searcher.getIndexReader().getTermFreqVector(self.base_doc + arg0, "text")
         for tv_term in tv.getTerms: self.terms.append(tv_term)
         self.doc_count+=1
