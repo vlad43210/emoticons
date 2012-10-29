7e3759bc24a2138fccd4e44d5d954e718ec07205
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-10-29 19:30:59 -0400

    debugging emoticon pmis

--- search_files.py
+++ search_files.py
@@ -64,7 +64,7 @@ def run(searcher, analyzer):
 
 
 if __name__ == '__main__':
-    STORE_DIR =  "/Volumes/TerraFirma/SharedData/vdb5/lucene_index"
+    STORE_DIR =  "/Volumes/TerraFirma/SharedData/vdb5/lucene_index_test"
     initVM(CLASSPATH)
     print 'lucene', VERSION
     directory = FSDirectory.getDirectory(STORE_DIR, False)
