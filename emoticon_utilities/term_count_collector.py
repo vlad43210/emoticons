baf3679f04cdfc94c3a8ca4e06a1c808cb95a970
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-10-29 16:30:49 -0400

    debugging emoticon pmis

--- emoticon_utilities/term_count_collector.py
+++ emoticon_utilities/term_count_collector.py
@@ -21,7 +21,7 @@ class TermCountCollector(PythonHitCollector):
         return True
 
     def collect(self, arg0, score):
-        tv = self.searcher.getIndexReader().getTermFreqVector(self.base_doc + arg0, "emoticons")
+        tv = self.searcher.getIndexReader().getTermFreqVector(self.base_doc + arg0, "text")
         for tv_term in tv.getTerms: self.terms.append(tv_term)
         self.doc_count+=1
 
