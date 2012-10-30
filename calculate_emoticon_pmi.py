b93c876ca975ab547f3ae0d353f053ea7125c993
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-10-30 19:56:13 -0400

    debugging emoticon pmis

--- calculate_emoticon_pmi.py
+++ calculate_emoticon_pmi.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     directory = FSDirectory.getDirectory(STORE_DIR, False)
     searcher = IndexSearcher(directory)
     analyzer = WhitespaceAnalyzer()
-    min_doc_frequency = 10
+    min_doc_frequency = 50
     emoticonPmiCalculator = PMICalculator(":)", searcher, analyzer)
     emoticonPmiCalculator.getTermPMI(min_doc_frequency)
     print "calculated PMI for :) at: ", time.time()
