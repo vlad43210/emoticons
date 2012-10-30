2b30cc56e1038ca3a04ab3095763a3fa4f32e7c1
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-10-30 09:24:04 -0400

    debugging emoticon pmis

--- index_tweets.py
+++ index_tweets.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     def fn():
         env.attachCurrentThread()
         start = datetime.now()
-        ix = IndexTweets(sys.argv[1], "/Volumes/TerraFirma/SharedData/vdb5/lucene_index_test",
+        ix = IndexTweets(sys.argv[1], "/Volumes/TerraFirma/SharedData/vdb5/lucene_index",
                    WhitespaceAnalyzer(Version.LUCENE_CURRENT), location_hash)
         #ix = IndexTweets(sys.argv[1], "/Volumes/TerraFirma/SharedData/vdb5/lucene_index_test",
         #           StandardAnalyzer(Version.LUCENE_CURRENT), location_hash)
