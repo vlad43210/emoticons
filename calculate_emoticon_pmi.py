29a7220bb22cb1e81dba35512408692c5ee9c82c
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-10-29 21:44:29 -0400

    debugging emoticon pmis

--- calculate_emoticon_pmi.py
+++ calculate_emoticon_pmi.py
@@ -60,7 +60,7 @@ class PMICalculator(object):
             if cnt%1000 == 0: print "processed term number: ", cnt, " out of: ", len(self.unique_terms)
 
         print "number of results: ", len(result_set)
-        sorted_result_set = sorted(list(result_set), lambda x: x.getPmi(), reverse=True)
+        sorted_result_set = sorted(list(result_set), lambda x: x.pmi, reverse=True)
         for tr in sorted_result_set: self.pmi_file.write(tr.getTerm() + "," + tr.getPmi() + "," + tr.getCooccurrenceCount() + "\n")
         self.pmi_file.close()
 
