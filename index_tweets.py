4a6c7b79877199816c5b47a6549185036afe04e1
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-11-10 15:06:53 -0500

    debugging PMI calculations

--- index_tweets.py
+++ index_tweets.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
         #ix = IndexTweets(sys.argv[1], "/Volumes/TerraFirma/SharedData/vdb5/lucene_index",
         #           WhitespaceAnalyzer(Version.LUCENE_CURRENT), location_hash)
         ix = IndexTweets(sys.argv[1], "/Volumes/TerraFirma/SharedData/vdb5/lucene_index_test",
-                   StandardAnalyzer(Version.LUCENE_CURRENT), location_hash)
+                   WhitespaceAnalyzer(Version.LUCENE_CURRENT), location_hash)
         ix.runIndexer()
         end = datetime.now()
         print end - start
