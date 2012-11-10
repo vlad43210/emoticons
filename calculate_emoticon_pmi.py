4e351f35891754fcaba2434e3ae030e7c11f1ee7
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-11-10 14:29:40 -0500

    debugging PMI calculations

--- calculate_emoticon_pmi.py
+++ calculate_emoticon_pmi.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     searcher = IndexSearcher(directory)
     analyzer = WhitespaceAnalyzer()
     min_doc_frequency = 500
-    emoticonPmiCalculator = PMICalculator(";o", searcher, analyzer)
+    emoticonPmiCalculator = PMICalculator(":)", searcher, analyzer)
     emoticonPmiCalculator.getTermPMI(min_doc_frequency)
-    print "calculated PMI for ;o at: ", time.time()
+    print "calculated PMI for :) at: ", time.time()
     searcher.close()
