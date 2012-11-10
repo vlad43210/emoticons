e40eae2b97f0bb8d004f00b6783d76cd9ff543f7
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-11-10 17:42:06 -0500

    debugging PMI calculations

--- index_tweets.py
+++ index_tweets.py
@@ -27,10 +27,10 @@ if __name__ == '__main__':
     def fn():
         env.attachCurrentThread()
         start = datetime.now()
-        #ix = IndexTweets(sys.argv[1], "/Volumes/TerraFirma/SharedData/vdb5/lucene_index",
-        #           WhitespaceAnalyzer(Version.LUCENE_CURRENT), location_hash)
-        ix = IndexTweets(sys.argv[1], "/Volumes/TerraFirma/SharedData/vdb5/lucene_index_test",
+        ix = IndexTweets(sys.argv[1], "/Volumes/TerraFirma/SharedData/vdb5/lucene_index",
                    WhitespaceAnalyzer(Version.LUCENE_CURRENT), location_hash)
+        #ix = IndexTweets(sys.argv[1], "/Volumes/TerraFirma/SharedData/vdb5/lucene_index_test",
+        #           WhitespaceAnalyzer(Version.LUCENE_CURRENT), location_hash)
         ix.runIndexer()
         end = datetime.now()
         print end - start
