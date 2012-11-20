1db838559ed587d21c40ce5a5842558da2210917
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2012-11-20 14:43:00 -0500

    debugging PMI

--- emoticon_utilities/term_count_collector.py
+++ emoticon_utilities/term_count_collector.py
@@ -52,7 +52,7 @@ class TermCountCollector(PythonHitCollector):
             print "eliminated duplicated string: ", tv_term_str.encode("ascii","ignore")
         else:
             for p_term in self.popular_terms_hash:
-                if p_term in tv_term_str and len(self.popular_terms_hash[p_term]) < 50:
+                if p_term in tv_term_str:
                     self.popular_terms_hash[p_term].append(tv_term_str)
             self.unique_tv_list[tv_term_str[:-1]] = 1
             try:
