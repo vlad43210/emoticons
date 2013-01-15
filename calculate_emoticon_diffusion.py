537e22a81838230b5eb856792443014b00758ba0
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2013-01-15 16:35:32 -0500

    adding emoticon diffusion analysis

--- calculate_emoticon_diffusion.py
+++ calculate_emoticon_diffusion.py
@@ -131,5 +131,5 @@ if __name__ == '__main__':
     analyzer = WhitespaceAnalyzer()
     #getBaselineStatistics()
     emoticon_list = [":)"]
-    for prop_emoticon in emoticon_list: calculateEmoticonDiffusion(prop_emoticon, searcher, analyzer, 3, 2)
+    for prop_emoticon in emoticon_list: calculateEmoticonDiffusion(prop_emoticon, searcher, analyzer, 1, 1)
     searcher.close()
