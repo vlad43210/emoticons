3fc7f43c2590b92825aa39c2b93e43a5f5ff9bbe
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2013-01-15 13:40:33 -0500

    adding emoticon diffusion analysis

--- calculate_emoticon_diffusion.py
+++ calculate_emoticon_diffusion.py
@@ -65,12 +65,12 @@ def calculateEmoticonDiffusion(emoticon, searcher, analyzer, usage_threshold = 1
     number_users = len(emoticon_users_by_time_hash)
     for uid in emoticon_users_by_time_hash:
         uidctr += 1
-        if uidctr%10==0: print "on uid number: ", uidctr, " out of: ", number_users, " at time: ", time.time()
+        if uidctr%100==0: print "on uid number: ", uidctr, " out of: ", number_users, " at time: ", time.time()
         if len(emoticon_users_by_time_hash[uid]) < usage_threshold: continue
         uquery = QueryParser("user_id_replied", analyzer).parse(uid)
         uhits = searcher.search(uquery)
         if uhits.length() == 0: continue
-        print "uid replied: ", uid, " number of hits: ", uhits.length()
+        #print "uid replied: ", uid, " number of hits: ", uhits.length()
 
         try:
             for uhit in uhits:
