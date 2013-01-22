98838a6bf3e0d7fa5df6307bc9e4fd1de1d7dd1e
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2013-01-22 13:18:25 -0500

    updating regex for final analysis

--- search_files.py
+++ search_files.py
@@ -33,7 +33,7 @@ def run(searcher, analyzer):
             for hit in hits:
                 hctr += 1
                 if hctr > hits.length()-2 or hctr > 100: break
-                print "hit id: ", hit.id, " term vector: ", searcher.getIndexReader().getTermFreqVector(hit.id, "text")
+                print "hit id: ", hit.get("id"), " term vector: ", searcher.getIndexReader().getTermFreqVector(hit.get("id"), "text")
                 print 'uid:', hit.get("user_id"), 'timestamp: ', hit.get("timestamp"), "country: ", hit.get('country'), "emoticons: ", hit.get('emoticons')
         except Exception, e: 
             print "failed to list hit: ", e
