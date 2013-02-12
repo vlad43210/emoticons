9b2fc6a3c03c80ca048b438ef714a2d3b9c110e5
Vladimir Barash <vdb5@Vladimir-Barashs-MacBook-Pro.local>
2013-02-11 20:51:00 -0500

    adding US tweets / day and JP tweets / day to propagation analysis

--- get_emoticon_propagation_curves.py
+++ get_emoticon_propagation_curves.py
@@ -32,13 +32,13 @@ def getBaselineStatistics(searcher, analyzer):
 
         #all tweets in day range US
         US_tweets_base_query = MatchAllDocsQuery()
-        us_escape_one = QueryParser("country", analyzer).escape("United")
+        #us_escape_one = QueryParser("country", analyzer).escape("United")
         #us_escape_two = 
-        #us_query = PhraseQuery()
-        #us_query.add(Term("country","United"))
-        #us_query.add(Term("country","States"))
+        us_query = PhraseQuery()
+        us_query.add(Term("country","Japan"))
+        #us_query.add(Term("country""States"))
         #US_tweets_country_query = us_query
-        US_tweets_country_query = QueryParser("country", analyzer).parse("United")
+        US_tweets_country_query = QueryParser("country", analyzer).parse(us_query)
         US_tweets_query_filter = QueryFilter(US_tweets_country_query)
         compound_filter_US_tweets = BooleanFilter()
         compound_filter_US_tweets.add(FilterClause(range_filter, BooleanClause.Occur.MUST))
